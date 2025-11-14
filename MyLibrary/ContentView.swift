da8cddafff2b03d9a0cb6b72b21ac31eb5fa87a8
Martin <marrty.h.1992@gmail.com>
2025-11-14 21:40:29 +0100

    Finished library app

--- MyLibrary/ContentView.swift
+++ MyLibrary/ContentView.swift
@@ -8,14 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
+    let library = Library()
+    
+    @State var searchText = ""
+    @State var alphabetical = false
+    
+    var filterLibrary: [MyLibrary] {
+        library.sort(by: alphabetical)
+        return library.search(for: searchText)
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            // List odkazuje na MyLibrary.swift
+            List(filterLibrary) { library in
+                NavigationLink {
+                    AuthorView(library: library)
+                } label: {
+                    // VStack pro autora a národnost z json (také z MyLibrary.swift)
+                    VStack(alignment: .leading) {
+                        Text(library.author)
+                            .font(.title2)
+                        
+                        Text(library.nationality)
+                            .font(.subheadline)
+                            .padding(.top, 3)
+                    }
+                }
+            }
+            .navigationTitle("Knihovna")
+            .searchable(text: $searchText)
+            .autocorrectionDisabled()
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button {
+                        withAnimation {
+                            alphabetical.toggle()
+                        }
+                    } label: {
+//                        if alphabetical {
+//                            Image(systemName: "person")
+//                        } else {
+//                            Image(systemName: "textformat")
+//                        }
+                        Image(systemName: alphabetical ? "person" : "textformat")
+                            .symbolEffect(.bounce, value: alphabetical)
+                    }
+                }
+            }
         }
-        .padding()
+        .preferredColorScheme(.dark)
     }
 }
 
